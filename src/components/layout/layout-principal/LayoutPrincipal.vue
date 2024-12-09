1d95563a98b0961c07d32036a539487f095ab984
FelipeSTW <feli.ahumadar@duocuc.cl>
2024-12-09 20:36:51 -0300

    feat: se crea layout principal

---
+++ src/components/layout/layout-principal/LayoutPrincipal.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { useLayoutPrincipal } from './layout-principal'
+import NavegacionApp from '@/components/common/navegacion/NavegacionApp.vue'
+
+const {} = useLayoutPrincipal()
+</script>
+
+<template src="./layout-principal.html"></template>
+<style src="./layout-principal.scss" lang="scss" scoped></style>
