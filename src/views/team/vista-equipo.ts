d51bfd96d61b467b8b89645fe0814d809cc32d7b
FelipeSTW <feli.ahumadar@duocuc.cl>
2024-12-09 20:27:37 -0300

    feat: se crea vista equipo

---
+++ src/views/team/vista-equipo.ts
@@ -0,0 +1,38 @@
+import { ref, onMounted } from 'vue'
+import { useRouter } from 'vue-router'
+import { useAlmacenPokemon } from '@/stores/almacenPokemon'
+import type { DetallePokemon } from '@/types/pokemon'
+
+export const useVistaEquipo = () => {
+  const router = useRouter()
+  const almacen = useAlmacenPokemon()
+  const mensajeAccion = ref<string>('')
+
+  const mostrarMensaje = (mensaje: string) => {
+    mensajeAccion.value = mensaje
+    setTimeout(() => {
+      mensajeAccion.value = ''
+    }, 3000)
+  }
+
+  const manejarEliminacion = (pokemon: DetallePokemon) => {
+    almacen.eliminarDeEquipo(pokemon.id)
+    mostrarMensaje(`${pokemon.nombre} ha sido eliminado del equipo`)
+  }
+
+  const navegarAInicio = () => {
+    router.push('/')
+  }
+
+  const hayEspacioDisponible = () => {
+    return almacen.equipoPokemon.length < almacen.LIMITE_EQUIPO
+  }
+
+  return {
+    almacen,
+    mensajeAccion,
+    manejarEliminacion,
+    navegarAInicio,
+    hayEspacioDisponible
+  }
+}
