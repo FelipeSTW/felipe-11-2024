02b7532be1ef17ffe2da9563827f50ca8d13446d
FelipeSTW <feli.ahumadar@duocuc.cl>
2024-12-09 21:26:19 -0300

    fix: se corrigen errores

--- src/components/pokemon/equipo-pokemon/EquipoPokemon.vue
+++ src/components/pokemon/equipo-pokemon/EquipoPokemon.vue
@@ -1,17 +1,22 @@
 <script setup lang="ts">
-import { useEquipoPokemon } from './equipo-pokemon'
+import { useAlmacenPokemon } from '@/stores/almacenPokemon'
 import ReproductorSonido from '../reproductor-sonido/ReproductorSonido.vue'
 import EstadisticasPokemon from '../estadisticas-pokemon/EstadisticasPokemon.vue'
+import type { DetallePokemon } from '@/types/pokemon'
 
-const {
-  equipo,
-  equipoVacio,
-  espaciosDisponibles,
-  eliminarDelEquipo,
-  verDetallePokemon,
-  obtenerDistribucionTipos,
-  obtenerPromedioEstadisticas
-} = useEquipoPokemon()
+const almacen = useAlmacenPokemon()
+
+defineProps<{
+  pokemonList: DetallePokemon[]
+}>()
+
+const emit = defineEmits<{
+  (e: 'eliminar', pokemon: DetallePokemon): void
+}>()
+
+function manejarEliminacion(pokemon: DetallePokemon) {
+  emit('eliminar', pokemon)
+}
 </script>
 
 <template src="./equipo-pokemon.html"></template>
