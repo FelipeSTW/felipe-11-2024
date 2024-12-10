11576be7fa978f9024a0fb72308b8fddb2173d5a
FelipeSTW <feli.ahumadar@duocuc.cl>
2024-12-09 21:20:57 -0300

    feat: se modificn las rutas

--- src/router/rutas.ts
+++ src/router/rutas.ts
@@ -1,24 +1,21 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { RouteRecordRaw } from 'vue-router'
 
-const rutas = createRouter({
-  history: createWebHistory(),
-  routes: [
-    {
-      path: '/',
-      name: 'inicio',
-      component: () => import('../views/home/VistaInicio.vue')
-    },
-    {
-      path: '/equipo',
-      name: 'equipo',
-      component: () => import('../views/team/VistaEquipo.vue')
-    },
-    {
-      path: '/equipo/:id',
-      name: 'detallePokemon',
-      component: () => import('../views/team/VistaPokemon.vue')
-    }
-  ]
-})
+const rutas: RouteRecordRaw[] = [
+  {
+    path: '/',
+    name: 'inicio',
+    component: () => import('../views/home/VistaInicio.vue')
+  },
+  {
+    path: '/equipo',
+    name: 'equipo',
+    component: () => import('../views/team/VistaEquipo.vue')
+  },
+  {
+    path: '/equipo/:id',
+    name: 'detallePokemon',
+    component: () => import('../views/team/VistaPokemon.vue')
+  }
+]
 
 export default rutas
