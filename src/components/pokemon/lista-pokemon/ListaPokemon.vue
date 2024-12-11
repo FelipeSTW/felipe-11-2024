e1c242fe859ee00facf2481ddce6d7458270d3f5
FelipeSTW <feli.ahumadar@duocuc.cl>
2024-12-11 18:26:25 -0300

    feat: scroll infinito

--- src/components/pokemon/lista-pokemon/ListaPokemon.vue
+++ src/components/pokemon/lista-pokemon/ListaPokemon.vue
@@ -3,6 +3,7 @@ import { useListaPokemon } from './lista-pokemon'
 import TarjetaPokemon from '../tarjeta-pokemon/TarjetaPokemon.vue'
 import { useAlmacenPokemon } from '@/stores/almacenPokemon'
 import type { DetallePokemon } from '@/types/pokemon'
+import { onMounted, onUnmounted } from 'vue'
 
 const { pokemonList, cargando, error, cargarSiguientePagina } = useListaPokemon()
 const almacenPokemon = useAlmacenPokemon()
@@ -10,6 +11,29 @@ const almacenPokemon = useAlmacenPokemon()
 const seleccionarPokemon = (pokemon: DetallePokemon) => {
   almacenPokemon.agregarAEquipo(pokemon)
 }
+
+let isScrolling = false;
+
+const onScroll = async () => {
+  if (isScrolling) return;
+
+  const scrollable = document.documentElement;
+  const nearBottom = scrollable.scrollTop + window.innerHeight >= scrollable.scrollHeight - 100;
+
+  if (nearBottom && !cargando.value) {
+    isScrolling = true;
+    await cargarSiguientePagina();
+    isScrolling = false;
+  }
+};
+
+onMounted(() => {
+  window.addEventListener('scroll', onScroll)
+})
+
+onUnmounted(() => {
+  window.removeEventListener('scroll', onScroll)
+})
 </script>
 
 <template src="./lista-pokemon.html"></template>
